2532b18089eb5e7f18b6eeb1b6eaa6e85a2d1473
NITHIN SAI ADUPA <96478917+saiadupa@users.noreply.github.com>
2025-03-06 18:04:13 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -295,7 +295,9 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a dynamic port
+    app.run(host="0.0.0.0", port=port, debug=True)
